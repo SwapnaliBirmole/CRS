7f1bbbbe7552759e3387321571a003db0fd5d33d
SWAPNALI_BIRMOLE <SWAPNALI_BIRMOLE@CMS.CO.IN>
2026-03-30 13:11:46 +0530

    Remove Profile menu

--- src/app/modules/auth/auth.component.ts
+++ src/app/modules/auth/auth.component.ts
@@ -1,13 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AngularSvgIconModule } from 'angular-svg-icon';
-import { NgClass } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
+
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css'],
-  imports: [AngularSvgIconModule, RouterOutlet, NgClass],
+  imports: [AngularSvgIconModule, RouterOutlet, CommonModule],
 })
 export class AuthComponent implements OnInit {
   constructor() {}
